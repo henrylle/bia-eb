b2d44dd8a904e49e08e6b7e4970d010576fc1d2d
Henrylle Maia <henryllemaia@gmail.com>
2023-04-20 19:29:05 -0300

    mudancas para o ambiente de dev da bia

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
-aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin [SEU_REPOSITORIO]
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 449892995723.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
-docker tag bia:latest [SEU_REPOSITORIO]/bia:$versao
-docker push [SEU_REPOSITORIO]/bia:$versao
+docker tag bia:latest 449892995723.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker push 449892995723.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
 rm .env 2> /dev/null
 ./gerar-compose.sh
-rm bia-versao-*zip
-zip -r bia-versao-$versao.zip docker-compose.yml
+rm bia-versao.zip
+zip -r bia-versao.zip docker-compose.yml
 git checkout docker-compose.yml
